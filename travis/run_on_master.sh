61e7ae39497af7268754e61d22687a36f0447be3
Nishant Srivastava <nisrulz@gmail.com>
2020-05-04 03:36:20 +0200

    use conditional stages from travis

--- travis/run_on_master.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Executing for master branch"
-
-./gradlew -q clean build assembleRelease
--- travis/run_on_non_pull_requests.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Executing for non-pull request i.e branch"
-
-./gradlew -q test
