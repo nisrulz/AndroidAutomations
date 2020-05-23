afbebf61def72dbb6546c6bd88cf072d4c2d834c
Nishant Srivastava <nisrulz@gmail.com>
2020-05-23 16:56:47 +0200

    added java example class and test for jacoco reporting - This makes sure jacoco is handling coverage for a project that has both java and kotlin classes

---
+++ app/src/main/java/com/github/nisrulz/example/androidautomations/MyJavaExample.java
@@ -0,0 +1,7 @@
+package com.github.nisrulz.example.androidautomations;
+
+public class MyJavaExample {
+    String getClassNameAsString() {
+        return this.getClass().getSimpleName();
+    }
+}
