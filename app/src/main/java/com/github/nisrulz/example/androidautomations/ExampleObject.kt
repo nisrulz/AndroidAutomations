5ed697df7db8a3146a3c4eaed6f4c16cdfdf0390
Nishant Srivastava <nisrulz@gmail.com>
2020-08-05 23:00:40 +0200

    added descriptiveFn

--- app/src/main/java/com/github/nisrulz/example/androidautomations/ExampleObject.kt
+++ app/src/main/java/com/github/nisrulz/example/androidautomations/ExampleObject.kt
@@ -2,6 +2,20 @@ package com.github.nisrulz.example.androidautomations
 
 object ExampleObject {
     fun getExampleString() = "Example"
+
+    fun descriptiveFn() {
+        println("This is going to be a descriptive function :] ")
+        println("Quick questions for you. Let's Go 🚀 ")
+        println("How much do you ♥️ Android? ${10*10}%")
+        println("-----")
+        println("What did it take to use Android Studio ? Everything 😬")
+        println("-----")
+        println("May the force be with you 💪")
+        println("Adios 👋")
+    }
 }
 
 fun String.appendExample() = "${this}Example"
+
+
+
--- app/src/main/java/com/github/nisrulz/example/androidautomations/MainActivity.kt
+++ app/src/main/java/com/github/nisrulz/example/androidautomations/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ExampleObject.descriptiveFn()
     }
 }
