e9f0cbc40e4cd55bd1a41675cdb48217feb208e4
Nishant Srivastava <nisrulz@gmail.com>
2022-08-19 00:59:17 +0200

    use view bindings

--- app/src/main/java/com/github/nisrulz/example/androidautomations/MainActivity.kt
+++ app/src/main/java/com/github/nisrulz/example/androidautomations/MainActivity.kt
@@ -2,12 +2,23 @@ package com.github.nisrulz.example.androidautomations
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.github.nisrulz.example.androidautomations.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding?.apply {
+            setContentView(root)
+        }
         ExampleObject.descriptiveFn()
+    }
+
+    override fun onDestroy() {
+        binding = null
+        super.onDestroy()
     }
 }
